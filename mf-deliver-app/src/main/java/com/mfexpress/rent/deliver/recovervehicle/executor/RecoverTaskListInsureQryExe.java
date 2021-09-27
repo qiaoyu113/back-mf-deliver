70c43f1c23320f67cb076b343b2d5d2ae787b2de
liusong <galigigi@foxmail.com>
2021-09-27 18:14:33 +0800

    修改批量退保列表bug

--- mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/recovervehicle/executor/RecoverTaskListInsureQryExe.java
+++ mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/recovervehicle/executor/RecoverTaskListInsureQryExe.java
@@ -1,10 +1,12 @@
 package com.mfexpress.rent.deliver.recovervehicle.executor;
 
 import com.mfexpress.component.dto.TokenInfo;
+import com.mfexpress.rent.deliver.api.SyncServiceI;
 import com.mfexpress.rent.deliver.constant.DeliverEnum;
 import com.mfexpress.rent.deliver.constant.JudgeEnum;
 import com.mfexpress.rent.deliver.dto.data.recovervehicle.RecoverQryListCmd;
 import com.mfexpress.rent.deliver.dto.data.recovervehicle.RecoverTaskListVO;
+import com.mfexpress.rent.deliver.dto.data.recovervehicle.RecoverVehicleVO;
 import com.mfexpress.rent.deliver.recovervehicle.RecoverQryServiceI;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -16,11 +18,14 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class RecoverTaskListInsureQryExe implements RecoverQryServiceI {
     @Resource
     private RecoverEsDataQryExe recoverEsDataQryExe;
+    @Resource
+    private SyncServiceI syncServiceI;
 
     @Override
     public RecoverTaskListVO execute(RecoverQryListCmd recoverQryListCmd, TokenInfo tokenInfo) {
@@ -31,6 +36,17 @@ public class RecoverTaskListInsureQryExe implements RecoverQryServiceI {
                 .must(QueryBuilders.matchQuery("isInsurance", JudgeEnum.NO.getCode()));
         FieldSortBuilder timeSortBuilder = SortBuilders.fieldSort("recoverVehicleTime").unmappedType("integer").order(SortOrder.DESC);
         fieldSortBuilderList.add(timeSortBuilder);
-        return recoverEsDataQryExe.getEsData(recoverQryListCmd, boolQueryBuilder, fieldSortBuilderList,tokenInfo);
+        RecoverTaskListVO recoverTaskListVO = recoverEsDataQryExe.getEsData(recoverQryListCmd, boolQueryBuilder, fieldSortBuilderList, tokenInfo);
+        if (recoverTaskListVO != null && recoverTaskListVO.getRecoverVehicleVOList() != null) {
+            List<String> serveNoList = recoverTaskListVO.getRecoverVehicleVOList().stream().map(RecoverVehicleVO::getServeNo).collect(Collectors.toList());
+            for (String serveNo : serveNoList) {
+                syncServiceI.execOne(serveNo);
+            }
+        }
+
+
+        return recoverEsDataQryExe.getEsData(recoverQryListCmd, boolQueryBuilder, fieldSortBuilderList, tokenInfo);
+
     }
+
 }
