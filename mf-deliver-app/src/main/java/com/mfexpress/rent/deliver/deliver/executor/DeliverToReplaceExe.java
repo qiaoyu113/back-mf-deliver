4aecc6f7f0ce3563ad916113a504398a94be1e00
lu_zheng <acanmail@126.com>
2021-12-07 13:44:25 +0800

    发车时替换车辆，如果车辆在在保状态，取保险开始时间到交付单中存储

--- mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/deliver/executor/DeliverToReplaceExe.java
+++ mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/deliver/executor/DeliverToReplaceExe.java
@@ -9,10 +9,13 @@ import com.mfexpress.rent.deliver.domainapi.ServeAggregateRootApi;
 import com.mfexpress.rent.deliver.dto.data.deliver.DeliverDTO;
 import com.mfexpress.rent.deliver.dto.data.deliver.DeliverReplaceCmd;
 import com.mfexpress.rent.deliver.dto.data.deliver.DeliverVehicleSelectCmd;
+import com.mfexpress.rent.deliver.utils.DeliverUtils;
 import com.mfexpress.rent.vehicle.api.VehicleAggregateRootApi;
+import com.mfexpress.rent.vehicle.api.VehicleInsuranceAggregateRootApi;
 import com.mfexpress.rent.vehicle.constant.ValidSelectStatusEnum;
 import com.mfexpress.rent.vehicle.data.dto.vehicle.VehicleInfoDto;
 import com.mfexpress.rent.vehicle.data.dto.vehicle.VehicleSaveCmd;
+import com.mfexpress.rent.vehicle.data.dto.vehicleinsurance.VehicleInsuranceDto;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -28,6 +31,9 @@ public class DeliverToReplaceExe {
     private DeliverAggregateRootApi deliverAggregateRootApi;
     @Resource
     private VehicleAggregateRootApi vehicleAggregateRootApi;
+
+    @Resource
+    private VehicleInsuranceAggregateRootApi vehicleInsuranceAggregateRootApi;
 
 
     public String execute(DeliverReplaceCmd deliverReplaceCmd) {
@@ -51,6 +57,7 @@ public class DeliverToReplaceExe {
         if (vehicleResult.getCode() != 0) {
             return vehicleResult.getMsg();
         }
+        VehicleInfoDto vehicleInfoDto = vehicleResult.getData();
         VehicleSaveCmd vehicleSaveCmd = new VehicleSaveCmd();
         vehicleSaveCmd.setId(Arrays.asList(deliverVehicleSelectCmd.get(0).getId()));
         vehicleSaveCmd.setSelectStatus(ValidSelectStatusEnum.CHECKED.getCode());
@@ -60,7 +67,14 @@ public class DeliverToReplaceExe {
         }
         //更换车辆信息 原交付单失效
 
-        deliverDTO.setIsInsurance(vehicleResult.getData().getInsuranceStatus().equals(JudgeEnum.YES.getCode()) ? JudgeEnum.YES.getCode() : JudgeEnum.NO.getCode());
+        if(JudgeEnum.YES.getCode().equals(vehicleInfoDto.getInsuranceStatus())){
+            deliverDTO.setIsInsurance(JudgeEnum.YES.getCode());
+            Result<VehicleInsuranceDto> vehicleInsuranceDtoResult = vehicleInsuranceAggregateRootApi.getVehicleInsuranceById(vehicleInfoDto.getId());
+            if(null != vehicleInsuranceDtoResult.getData()){
+                deliverDTO.setInsuranceStartTime(DeliverUtils.getYYYYMMDDByString(vehicleInsuranceDtoResult.getData().getStartTime()));
+            }
+        }
+
         deliverDTO.setServeNo(deliverReplaceCmd.getServeList().get(0));
         deliverDTO.setCarNum(deliverVehicleSelectCmd.get(0).getPlateNumber());
         deliverDTO.setCarId(deliverVehicleSelectCmd.get(0).getId());
