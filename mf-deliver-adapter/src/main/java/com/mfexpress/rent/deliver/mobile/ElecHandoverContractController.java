beb7a1483eaff7524772288b6562b31f803ee032
lu_zheng <acanmail@126.com>
2021-12-09 15:12:08 +0800

    交付项目增加契约锁流程，开发分支初始提交

---
+++ mf-deliver-adapter/src/main/java/com/mfexpress/rent/deliver/mobile/ElecHandoverContractController.java
@@ -0,0 +1,14 @@
+package com.mfexpress.rent.deliver.mobile;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// elec 是 electric/electronic 的缩写
+@RestController
+@RequestMapping("/api/deliver/v3/elecHandoverContract")
+@Api(tags = "api--交付--1.1电子交接合同", value = "ElecHandoverContractController")
+public class ElecHandoverContractController {
+
+    
+}
