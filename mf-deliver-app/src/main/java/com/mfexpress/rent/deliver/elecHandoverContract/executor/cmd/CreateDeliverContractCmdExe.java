dc1eea6d43850e70bb13e9cc629646a12366622b
韩振琦 <HHHHHzq@outlook.com>
2022-02-22 14:58:34 +0800

    +修改拦截限制

--- mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/elecHandoverContract/executor/cmd/CreateDeliverContractCmdExe.java
+++ mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/elecHandoverContract/executor/cmd/CreateDeliverContractCmdExe.java
@@ -309,7 +309,7 @@ public class CreateDeliverContractCmdExe {
     }
 
     private void checkDate(CreateDeliverContractCmd cmd) {
-        if (DateUtil.between(cmd.getDeliverInfo().getDeliverVehicleTime(), new Date(), DateUnit.DAY) >= 6) {
+        if (DateUtil.between(cmd.getDeliverInfo().getDeliverVehicleTime(), new Date(), DateUnit.DAY) > 6) {
             log.info("发车日期超出可选范围  参数:{}", cmd);
             throw new CommonException(ResultErrorEnum.VILAD_ERROR.getCode(), "发车日期超出可选范围");
         }
