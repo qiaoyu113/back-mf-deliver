4aec2f90c3a50026ba253e328e11ee78e9332c07
wuzifan <1428446145@qq.com>
2022-06-08 16:10:12 +0800

    修改待退保后无租赁方式bug

--- mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/recovervehicle/executor/RecoverTaskListCompletedQryExe.java
+++ mf-deliver-app/src/main/java/com/mfexpress/rent/deliver/recovervehicle/executor/RecoverTaskListCompletedQryExe.java
@@ -1,9 +1,12 @@
 package com.mfexpress.rent.deliver.recovervehicle.executor;
 
 import com.mfexpress.component.dto.TokenInfo;
+import com.mfexpress.component.response.Result;
 import com.mfexpress.rent.deliver.constant.*;
+import com.mfexpress.rent.deliver.domainapi.ServeAggregateRootApi;
 import com.mfexpress.rent.deliver.dto.data.recovervehicle.RecoverQryListCmd;
 import com.mfexpress.rent.deliver.dto.data.recovervehicle.RecoverTaskListVO;
+import com.mfexpress.rent.deliver.dto.data.serve.ServeDTO;
 import com.mfexpress.rent.deliver.recovervehicle.RecoverQryServiceI;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -20,7 +23,8 @@ import java.util.List;
 public class RecoverTaskListCompletedQryExe implements RecoverQryServiceI {
     @Resource
     private RecoverEsDataQryExe recoverEsDataQryExe;
-
+    @Resource
+    private ServeAggregateRootApi serveAggregateRootApi;
     @Override
     public RecoverTaskListVO execute(RecoverQryListCmd recoverQryListCmd, TokenInfo tokenInfo) {
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
@@ -33,6 +37,10 @@ public class RecoverTaskListCompletedQryExe implements RecoverQryServiceI {
         fieldSortBuilderList.add(updateTimeSortBuilder);
         RecoverTaskListVO recoverTaskListVO = recoverEsDataQryExe.getEsData(recoverQryListCmd, boolQueryBuilder, fieldSortBuilderList, tokenInfo, Constants.ES_DELIVER_INDEX, Constants.ES_DELIVER_TYPE);
         recoverTaskListVO.getRecoverVehicleVOList().forEach(recoverVehicleVO -> {
+            Result<ServeDTO> serveDtoByServeNo = serveAggregateRootApi.getServeDtoByServeNo(recoverVehicleVO.getServeNo());
+
+            recoverVehicleVO.setLeaseModelDisplay(LeaseModelEnum.getEnum(serveDtoByServeNo.getData().getLeaseModelId()).getName());
+            recoverVehicleVO.setLeaseModelId(serveDtoByServeNo.getData().getLeaseModelId());
             if (JudgeEnum.YES.getCode().equals(recoverVehicleVO.getRecoverAbnormalFlag())) {
                 recoverVehicleVO.setRecoverTypeDisplay(RecoverVehicleType.ABNORMAL.getName());
             } else {
