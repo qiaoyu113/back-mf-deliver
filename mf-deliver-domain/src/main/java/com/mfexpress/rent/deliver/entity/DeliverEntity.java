a786db11684a9761c3b636448c8e8dcd8bdc7957
lu_zheng <acanmail@126.com>
2022-08-30 11:11:16 +0800

    1、投保申请时间日期转换bug修复

--- mf-deliver-domain/src/main/java/com/mfexpress/rent/deliver/entity/DeliverEntity.java
+++ mf-deliver-domain/src/main/java/com/mfexpress/rent/deliver/entity/DeliverEntity.java
@@ -361,7 +361,7 @@ public class DeliverEntity implements DeliverEntityApi {
             return null;
         }
 
-        InsuranceApplyDTO insuranceApplyDTO = BeanUtil.toBean(applyPO, InsuranceApplyDTO.class);
+        InsuranceApplyDTO insuranceApplyDTO = BeanUtil.toBeanIgnoreError(applyPO, InsuranceApplyDTO.class);
         if (!StringUtils.isEmpty(applyPO.getApplyTime())) {
             insuranceApplyDTO.setApplyTime(DateUtil.parseDateTime(applyPO.getApplyTime()));
         }
